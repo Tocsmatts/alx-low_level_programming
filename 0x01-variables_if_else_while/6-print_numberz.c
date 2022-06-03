0bf916ddbe8255b612061156406b4d509e1b9609
Tocsmatts <toochukwuezeobi@gmail.com>
2022-06-03 13:59:59 -0700

    Adding 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print single digit numbers
+ *
+ * Return: Returns 0 at success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
