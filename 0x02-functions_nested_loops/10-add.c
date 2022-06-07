0301527ff434e1cf81cd26746f5b779628b09828
Tocsmatts <toochukwuezeobi@gmail.com>
2022-06-07 15:13:41 -0700

    Adding 10-add.c and related files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - code to add two figures
+ *
+ * @a: Integer
+ *
+ * @b: Integer
+ *
+ * Return: Return 0 on success
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
