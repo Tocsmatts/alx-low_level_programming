ec9fb6f8b6f6db80779f34322d18d02fede8da71
Tocsmatts <toochukwuezeobi@gmail.com>
2022-06-07 10:38:23 -0700

    Adding 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Program that prints the lowercase alphabet
+ *
+ * Return: Return 0 on success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+		_putchar(a);
+	_putchar('\n');
+	return (0);
+}
