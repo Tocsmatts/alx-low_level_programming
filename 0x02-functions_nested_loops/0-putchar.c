e1b8f57221dd969fe6be4ee83e9eaa8d145cc7f4
Tocsmatts <toochukwuezeobi@gmail.com>
2022-06-07 10:25:22 -0700

    Adding changes to files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - program to print _putchar
+ * main - program to print _putchar as messsage
  *
  * Return: Return 0 on success
  */
@@ -15,14 +15,14 @@ int main(void)
 	char a = 'a';
 	char r = 'r';
 
-	putchar('_');
-	putchar(p);
-	putchar(u);
-	putchar(t);
-	putchar(c);
-	putchar(h);
-	putchar(a);
-	putchar(r);
-	putchar('\n');
+	_putchar('_');
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	_putchar('\n');
 	return (0);
 }
