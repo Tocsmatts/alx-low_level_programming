f091d09ecdc807a40e772f905daae0d1c3c159a6
Tocsmatts <toochukwuezeobi@gmail.com>
2022-06-13 10:55:39 -0700

    Adding files for 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen  - returns length of string
+ *
+ * @s: s is a char
+ * Return: Return strlen on sucess
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
