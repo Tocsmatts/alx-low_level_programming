d572263e4f27f88d032638b8ce7a1f401dd27ded
Tocsmatts <toochukwuezeobi@gmail.com>
2022-06-13 13:14:31 -0700

    Adding files

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - prints reverse of a string
+ *
+ * @s: s is a char
+ */
+void print_rev(char *s)
+{
+	printf("%s", strrev(s));
+}
