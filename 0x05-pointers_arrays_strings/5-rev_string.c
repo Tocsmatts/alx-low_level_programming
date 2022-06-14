96262fafb6ac1bd3527fb77e682b344a1060f8b1
Tocsmatts <toochukwuezeobi@gmail.com>
2022-06-14 13:30:27 -0700

    Adding files for 5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - prints reverse of a string
+ *
+ * @s: s is a char
+ */
+void rev_string(char *s)
+{
+	char rev[1000];
+	int a, b, count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	b = count - 1;
+	for (a = 0; a < count; a++)
+	{
+		rev[a] = s[b];
+		b--;
+	}
+	printf("%s\n", rev);
+	s = "";
+}
