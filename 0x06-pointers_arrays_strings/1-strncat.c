97a64aeb76e8147b3892d51ba3effd4568936c39
Tocsmatts <toochukwuezeobi@gmail.com>
2022-06-16 13:55:25 -0700

    Adding files

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,12 +11,7 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		*src = src[i];
-		dest = strcat(dest, src);
-	}
+	*src = src[n];
+	dest = strcat(dest, src);
 	return (dest);
 }
