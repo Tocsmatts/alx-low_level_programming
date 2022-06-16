e34d8251b442df3a6c4588a90f9cbc17b4bc51cb
Tocsmatts <toochukwuezeobi@gmail.com>
2022-06-16 14:56:50 -0700

    Adding files for 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compares strings
+ *
+ * @s1: s1 is a char
+ * @s2: s2 is a char
+ * Return: Returns s1
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
