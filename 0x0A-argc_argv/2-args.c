ae37b111e36365eb1527974707715a76d6ae2bc3
Tocsmatts <toochukwuezeobi@gmail.com>
2022-06-24 08:13:13 -0700

    Adding files for 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main file
+ * @argc: counts arguments
+ * @argv: arguement vector
+ * Return: Return 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+	{
+		printf("%s\n", argv[count]);
+	}
+	return (0);
+}
