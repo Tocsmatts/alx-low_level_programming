6a1c45e11daebda8652dc7330e2a373b9365c32c
Tocsmatts <toochukwuezeobi@gmail.com>
2022-06-30 06:11:00 -0700

    Adding files for 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: unsigned int
+ * Return: Return i
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *i;
+
+	i = malloc(b);
+	if (i == NULL)
+		exit(98);
+	return (i);
+}
