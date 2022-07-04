43c1b34723aa10315a3216017ec4af8f50804845
Tocsmatts <toochukwuezeobi@gmail.com>
2022-07-04 14:26:54 -0700

    Adding file for 1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,9 +13,12 @@
  */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+	if (d)
+	{
+		char *name;
+		float age;
+		char *owner;
+	}
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
