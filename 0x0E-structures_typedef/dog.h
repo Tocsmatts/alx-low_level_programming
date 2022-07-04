edfe43d4f064dce45388d9124538224518d97950
Tocsmatts <toochukwuezeobi@gmail.com>
2022-07-04 14:02:42 -0700

    Adding file for dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,14 @@
 
 #include <stdlib.h>
 
+/**
+ * struct dog - This is a header file
+ * @name: Name of dog
+ * @age: age of dog
+ * @owner: Owner of dog
+ *
+ * Description: This is a struct file for dog
+ */
 struct dog
 {
 	char *name;
